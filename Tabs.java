2e92c445458d93e38163de06fc8021db5d1d31f4
haleyyounginfo <haleyyoung.info@gmail.com>
2016-03-07 12:25:26 -0800

    Example of why tabs are bad

---
+++ Tabs.java
@@ -0,0 +1,15 @@
+class Tabs {
+	public static void main(String args[]) {
+		// This is a file with tabs in it
+	  int i = 0;  // Dummy integer
+		int j = 1;	// Another dummy integer
+	}
+	public static void helperMethod() {
+	  String s = "Hi";
+	  if (s.equals("bye")) {
+	  	// TODO fix
+	  }	else {
+	    // Set it to bye
+	  }
+	}
+}
